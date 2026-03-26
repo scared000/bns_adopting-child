dc081142eedee249098b3b5912eb9d64cbb77e20
Khenjie Apostol <khenjieapostol3@gmail.com>
2026-03-26 10:01:08 +0800

    add column in immunization table

--- database/migrations/2026_03_18_054128_create_immunizations_table.php
+++ database/migrations/2026_03_18_054128_create_immunizations_table.php
@@ -11,10 +11,12 @@ return new class extends Migration
         Schema::create('immunizations', function (Blueprint $table) {
             $table->id();
             $table->foreignId('child_id')->constrained('adopted_children')->cascadeOnDelete();
-            $table->string('description')->nullable();
-            $table->boolean('first_dose')->default(false);
-            $table->boolean('second_dose')->default(false);
-            $table->boolean('third_dose')->default(false);
+            $table->string('vaccine_description');
+            $table->date('dose_1')->nullable();
+            $table->date('dose_2')->nullable();
+            $table->date('dose_3')->nullable();
+            $table->string('status')->default('incomplete');
+            $table->text('remarks')->nullable();
             $table->timestamps();
         });
     }
