c7fe57e1e908f1d1321b7f6ab2efbd4c83772f2a
Khenjie Apostol <khenjieapostol3@gmail.com>
2026-04-15 17:35:30 +0800

    add forbidden error handler

---
+++ lang/vendor/filament-error-pages/id/error-pages.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'home' => 'Kembali ke beranda',
+    'previous' => 'Kembali ke halaman sebelumnya',
+    '404' => [
+        'title' => 'Halaman tidak ditemukan.',
+        'description' => 'Halaman mungkin telah dihapus, namanya diubah, atau sedang tidak tersedia.',
+    ],
+    '403' => [
+        'title' => 'Halaman tidak dapat diakses.',
+        'description' => 'Anda tidak memiliki izin untuk mengakses halaman ini.',
+    ],
+];
